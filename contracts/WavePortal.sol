0d090817559fd991264869a08690a2755825c2ea
Insuline <insuline.ru@gmail.com>
2021-11-06 23:05:12 +0300

    feat(contract): создание контракта и базовый функционал

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+    // TODO: Переписать на mapping
+    address[] waveSenders;
+
+    constructor() {
+        console.log("Yo yo, I am a contract am I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        // TODO: Переписать на mapping
+        waveSenders.push(msg.sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getWaveSendersCount() public view returns (uint256) {
+        console.log("We have %d total senders!", waveSenders.length);
+        return waveSenders.length;
+    }
+}
